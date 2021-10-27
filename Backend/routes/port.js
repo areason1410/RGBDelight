aa57e4c530e92d8babfc5836e7b6513fe03cd29d
Aaron Reason <aaronreason@hotmail.co.uk>
2021-10-27 14:35:32 +0100

    Arduino code finished, created server for simple serial communication to the arduino

---
+++ Backend/routes/port.js
@@ -0,0 +1,13 @@
+const serialPort = require("serialport");
+const port = new serialPort("/dev/ttyACM0",{
+    baudRate: 9600
+});
+const express = require("express");
+const router = express.Router();
+
+
+router.post("/changeColour", async(req, res) => {
+    port.write(`${req.body.red}, ${req.body.green}, ${req.body.blue}`);
+})
+
+module.exports = router;
