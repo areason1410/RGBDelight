ec33f8b591672629c4b7ef592103b09483231e29
bmajor65 <benmajor6565@yahoo.com>
2022-01-10 18:34:18 +0000

    .

--- Frontend/resources/JS/createRooms.js
+++ Frontend/resources/JS/createRooms.js
@@ -2,12 +2,13 @@
   document.getElementById("createRoom").addEventListener("click", () => {
         fetch("http://localhost:3000/database/createRoom", {
               method: "POST",
-              headers: { 'Content-Type': 'applictaion/json' },
+              headers: { 'Content-Type': 'application/json' },
               body: JSON.stringify({
                     "roomid": 10,
                     "roomname": "test"
               })
         }).then(res => {
+              
               console.log("Request complete! response:", res);
         });
   })
