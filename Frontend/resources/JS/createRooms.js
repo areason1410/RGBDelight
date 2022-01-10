c0cd403adf970ec9b20af3a7d556d396541fc2f8
bmajor65 <benmajor6565@yahoo.com>
2022-01-10 19:12:07 +0000

    add room function now works

--- Frontend/resources/JS/createRooms.js
+++ Frontend/resources/JS/createRooms.js
@@ -1,15 +1,11 @@
 
-  document.getElementById("createRoom").addEventListener("click", () => {
-      var roomInput = document.createElement("input");
-      roomInput.setAttribute('type', 'text')
-      
-      
+  const roomNameInput = document.getElementById('createRoomInput')
+  document.getElementById("newRoom").addEventListener("click", () => {
       fetch("http://localhost:3000/database/createRoom", {
               method: "POST",
               headers: { 'Content-Type': 'application/json' },
               body: JSON.stringify({
-                    "roomid": 10,
-                    "roomname": "test"
+                    "roomname": roomNameInput.value
               })
         }).then(res => {
               
