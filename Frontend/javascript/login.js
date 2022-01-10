69fe27c9198ace4d718b83b418d20857716bed9c
MichaelJohnEdson <MichaelJohnEdson@gmail.com>
2022-01-10 14:10:39 +0000

    new login

--- Frontend/javascript/login.js
+++ Frontend/javascript/login.js
@@ -1,6 +1,21 @@
-let Login = document.getElementById(Login)
+const emailInput = document.getElementById("emailInput");
+const password = document.getElementById("password")
+
+document.getElementById("createAccount").addEventListener("click", () => {
+    fetch("http://localhost:3000/database/addUser", {
+        method: "POST",
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            "username": emailInput.value,
+            "password": password.value
+        })
+    }).then(res => {
+        console.log("Request complete! response:", res);
+    });
+
+})
 
 
-function login() {
-    onclick
+function loginPage() {
+    location.href = "loginPage.html"
 }
