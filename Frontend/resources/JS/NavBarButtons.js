c60541f0e83c89026e16a2a3b97d78526da67a5a
bmajor65 <benmajor6565@yahoo.com>
2022-01-10 17:56:38 +0000

    Creating rooms

--- Frontend/resources/JS/NavBarButtons.js
+++ Frontend/resources/JS/NavBarButtons.js
@@ -1,18 +1,16 @@
-// let button1 = document.getElementById("scenes").addEventListener("click", () => {
-//     window.location.replace("scenes.html")
-//   })
+let button1 = document.getElementById("scenes").addEventListener("click", () => {
+    window.location.replace("scenes.html")
+  })
 
-// let button2 = document.getElementById("home").addEventListener("click", () => {
-//     window.location.replace("homePage.html")
-//   })
+let button2 = document.getElementById("home").addEventListener("click", () => {
+    window.location.replace("homePage.html")
+  })
 
-// let button3 = document.getElementById("settings").addEventListener("click", () => {
-//     window.location.replace("settings.html")
-//   })
+let button3 = document.getElementById("settings").addEventListener("click", () => {
+    window.location.replace("settings.html")
+  })
 
-// let button4 = document.getElementById("createRoom").addEventListener("click", () => {
-//     window.location.replace("newroom.html")
-//   })
+
   
 
 
